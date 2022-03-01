8df8501ac28095391661820c6d4cc8979394cee3
Ng kai sheng Ansonm <nansonm@u90z03s02.42kl.edu.my>
2022-03-01 17:14:04 +0800

    after norminetted =/

--- get_next_line.c
+++ get_next_line.c
@@ -1,77 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nansonm <ansonmng@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 16:18:12 by nansonm           #+#    #+#             */
+/*   Updated: 2022/03/01 16:23:13 by nansonm          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include "get_next_line.h"
 
-static void reading(int fd, char *buff, char **str)
+static void	reading(int fd, char *buff, char **str)
 {
-    int     i;
-    char    *tmp;
+	int		i;
+	char	*tmp;
 
-    i = 0;
-    if (!*str || !ft_strchr(*str, '\n'))
-    {
-        i = read(fd, buff, BUFFER_SIZE);
-        while (i > 0)
-        {
-            buff[i] = '\0';
-            if (!*str)
-                *str = ft_strdup("");
-            tmp = *str;
-            *str = ft_strjoin(*str, buff);
-            free (tmp);
-            if (ft_strchr(buff, '\n'))
-                break;
-            i = read(fd, buff, BUFFER_SIZE);
-        }
-    }
-    free(buff);
-}
-
-static char *process(char  **str)
-{
-    int     i;
-    int     j;
-    char    *tmp;
-    char    *save;
-
-    if (!*str)
-        return (NULL);
-    if (!ft_strchr(*str, '\n'))
-    {
-        save = ft_substr(*str, 0, ft_strlen(*str));
-        free(*str);
-        *str = NULL;
-        return (save);
-    }
-        i = ft_strlen(*str);
-        j = ft_strlen(ft_strchr(*str, '\n'));
-        save = ft_substr(*str, 0, i - j + 1);
-        tmp = *str;
-        *str = ft_substr(ft_strchr(*str, '\n'), 1, j - 1);
-        free(tmp);
-        if (!**str)
+	i = 0;
+	if (!*str || !ft_strchr(*str, '\n'))
+	{
+		i = read(fd, buff, BUFFER_SIZE);
+		while (i > 0)
 		{
-			free(*str);
-			*str = NULL;
+			buff[i] = '\0';
+			if (!*str)
+				*str = ft_strdup("");
+			tmp = *str;
+			*str = ft_strjoin(*str, buff);
+			free (tmp);
+			if (ft_strchr(buff, '\n'))
+				break ;
+			i = read(fd, buff, BUFFER_SIZE);
 		}
-    return (save);
+	}
+	free(buff);
 }
 
-char    *get_next_line(int fd)
+static char	*process(char **str)
 {
-    char    *buff;
-    static char *str;
+	char	*tmp;
+	char	*save;
 
-    buff = malloc(sizeof(char) * ((BUFFER_SIZE) + 1));
-    if (!buff)
-        return (0);
-    if (BUFFER_SIZE < 1 || fd == -1 || read(fd, buff, 0) == -1)
-    {
-        free (buff);
-        return (0);
-    }
-    reading(fd, buff, &str);
-    return (process(&str));
+	if (!*str)
+		return (NULL);
+	if (!ft_strchr(*str, '\n'))
+	{
+		save = ft_substr(*str, 0, ft_strlen(*str));
+		free(*str);
+		*str = NULL;
+		return (save);
+	}
+	save = ft_substr(*str, 0, ft_strlen(*str)
+			- ft_strlen(ft_strchr(*str, '\n')) + 1);
+	tmp = *str;
+	*str = ft_substr(ft_strchr(*str, '\n'), 1,
+			ft_strlen(ft_strchr(*str, '\n')) - 1);
+	free(tmp);
+	if (!**str)
+	{	
+		free(*str);
+		*str = NULL;
+	}
+	return (save);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buff;
+	static char	*str;
+
+	buff = malloc(sizeof(char) * ((BUFFER_SIZE) + 1));
+	if (!buff)
+		return (0);
+	if (BUFFER_SIZE < 1 || fd == -1 || read(fd, buff, 0) == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	reading(fd, buff, &str);
+	return (process(&str));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nansonm <ansonmng@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 16:18:14 by nansonm           #+#    #+#             */
+/*   Updated: 2022/03/01 16:18:16 by nansonm          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -5,14 +17,14 @@
 #  define BUFFER_SIZE 42
 # endif
 
-#include <stdlib.h>
+# include <stdlib.h>
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 
-char    *ft_strchr(const char *s, int c);
-char    *ft_substr(const char *s, unsigned int start, size_t len);
-size_t  ft_strlen(const char *s);
-char    *ft_strjoin(char *s1, char *s2);
-char    *get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
 #endif
